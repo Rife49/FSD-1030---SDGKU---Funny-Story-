a6135b290bb2a19f95c4b9ff06ade1f11ce52ca5
Robert Rife <rife@Roberts-MacBook-Pro.local>
2026-03-03 16:08:02 -0800

    funny Story

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+// 1. Declare Variables
+// Using let
+let character = "luke";
+let action = "jump into the lake";
+let object = "fish";
+let place = "the lake";
+let time = "a sunny morning";
+// Using "const"
+const occasion = "fishing competition";
+// 2. Create the Story
+console. log ("--- Funny Story ---");
+console. log(character + " was at " + place + " on " + time + "."); console. log ("Suddenly " + character + " saw " + object + " and decided to " + action +".");
+console. log ("It turned out to be " + occasion + " and everyone laughed!");
+// 3. Experiment with Different Values
